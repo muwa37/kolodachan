2020666a69c4d59f89386fabc1b5636ee835fe69
nkyume <nekoyume2003@gmail.com>
2024-03-01 12:33:21 +1000

    fastapi app

---
+++ server/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+favicon_path = "favicon.ico"
+
+
+@app.get("/{board}/")
+async def get_board(board, t_skip=0, t_step=10, p_limit=3):
+    tmp_threads = board[t_skip:t_skip + t_step]
+    threads = []
+    for thread in tmp_threads:
+        threads.append(thread[0:1:] + thread[-p_limit::])
+    return threads
+
+
+@app.get("/{board}/{thread}")
+async def get_thread(board, thread):
+    """Return thread"""
+    return
+
+
+@app.get("/{board}/{thread}/{post}")
+async def get_post(board, thread, post):
+    return {"board": board, "thread": thread, "post": post}
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    file_path = "favicon.ico"
+    return FileResponse(path=file_path)
