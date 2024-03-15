d93ac66963ce0f02a10510d15fee3137c46d7f01
nkyume <nekoyume2003@gmail.com>
2024-03-15 21:50:33 +1000

    added 404 status code

--- server/src/router/file.py
+++ server/src/router/file.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+import os
+
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
 router = APIRouter(prefix='/file', tags=['file'])
 
+
 @router.get('/', response_class=FileResponse)
 async def get_file(name: str):
     path = f'../files/{name}'
+    dir = os.listdir('../files')
+    if name not in dir:
+        raise HTTPException(status_code=404, detail='File does not exist')
     return path
