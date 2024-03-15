9047ffb871eadf824bc9dac200aef8a0c2a2a207
muwa37 <yond41m3@icloud.com>
2024-03-15 23:49:50 +0500

    Modified home page

--- client/src/components/wrapper/Navbar.jsx
+++ client/src/components/wrapper/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import kolodaNavLogo from '../../assets/images/static/koloda-nav-icon.png';
 
 const Navbar = () => {
   return (
     <header className='flex items-center'>
-      <nav className='w-full flex space-x-6 ml-8 items-center justify-between'>
-        <Link to='/'>kolodaChan</Link>
+      <nav className='w-full h-full flex space-x-6 items-center justify-between'>
+        <Link to='/'>
+          <div className='flex h-full items-center justify-center'>
+            <div className='flex justify-center items-center w-1/2 h-full'>
+              <img
+                className='h-10 saturate-50 opacity-75 drop-shadow-2xl '
+                src={kolodaNavLogo}
+                alt='koloda-nav-logo'
+              />
+            </div>
+            <h1>kolodaChan</h1>
+          </div>
+        </Link>
         <nav className='w-1/4 flex items-center justify-evenly'>
           <Link to='/boards'>boards</Link>
           <Link to='/threads'>threads</Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import kolodaHomeImg from '../assets/images/static/koloda-home.png';
 
 export const Home = () => {
-  return <div>home page</div>;
+  return (
+    <section className='flex w-full h-full items-center justify-evenly'>
+      <div className='h-5/6 flex flex-col justify-evenly items-center'>
+        <h1 className='text-6xl'>kolodaChan</h1>
+        <div>
+          <h2 className='text-2xl'>welcome, nya^^</h2>
+          <p>kolodaChan - borda dlya kavainih lydey</p>
+        </div>
+      </div>
+      <div className='flex justify-center items-center w-1/2 h-full'>
+        <img
+          className='w-5/6 saturate-50 opacity-75 drop-shadow-2xl blur-[0.5px]'
+          src={kolodaHomeImg}
+          alt='koloda-homepage'
+        />
+      </div>
+    </section>
+  );
 };
