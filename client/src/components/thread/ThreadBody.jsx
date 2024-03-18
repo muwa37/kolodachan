9fc79887a064017ec8ff50dd30fc7e30d8f9f7f2
nuwa37 <yond41m3@icloud.com>
2024-03-18 14:16:04 +0500

    Modified layout

--- client/src/components/thread/ThreadBody.jsx
+++ client/src/components/thread/ThreadBody.jsx
@@ -4,11 +4,14 @@ export const ThreadBody = ({ title, text, attachments }) => {
   return (
     <div className='w-full flexitems-start justify-start'>
       <h3 className='text-2xl font-semibold'>{title}</h3>
-      <div className='flex'></div>
-      {attachments.map(item => (
-        <div className='p-2'>thread attachments</div>
-      ))}
-
+      <div className='flex'>
+        {attachments.map(item => (
+          <div className='p-2 flex flex-col justify-center items-center max-w-1/4'>
+            <p>{item.name}</p>
+            <img src={item.link} />
+          </div>
+        ))}
+      </div>
       <p>{text}</p>
     </div>
   );
