ac7d871d3aca247f99d3d86a332114d9fe5d84c0
nkyume <nekoyume2003@gmail.com>
2024-05-19 16:14:43 +1000

    added user router to api

--- server/src/kolodachan/api.py
+++ server/src/kolodachan/api.py
@@ -1,14 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from kolodachan.auth import auth
 from kolodachan.database import PostgreInterface
-from kolodachan.router import board, comment, file, thread
+from kolodachan.router import board, comment, file, thread, user
 from kolodachan.util.files import FileHandler
+
+load_dotenv()
+db_url = os.getenv('DB_URL')
 
 app = FastAPI()
 
 origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -23,6 +29,8 @@ async def free_bitcoins():
     return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
+app.include_router(auth.router)
+app.include_router(user.router)
 app.include_router(board.router)
 app.include_router(thread.router)
 app.include_router(comment.router)
@@ -32,5 +40,10 @@ app.include_router(file.router)
 @app.on_event('startup')
 async def startup_event():
     app.state.db = PostgreInterface()
-    await app.state.db.connect('config.toml')
+    await app.state.db.connect(db_url)
     app.state.filehandler = FileHandler()
+
+
+@app.on_event('shutdown')
+async def shutdown_event():
+    await app.state.db.disconnect()
