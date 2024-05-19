15db1471547aa8233016638b8c9a2bdbda493198
nkyume <nekoyume2003@gmail.com>
2024-05-19 17:50:49 +1000

    fixed activatevenv

--- server/activatevenv.sh
+++ server/activatevenv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 custom_activate(){
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+  echo "Script directory: $SCRIPT_DIR"
   source ".venv/bin/activate"
-  export PYTHONPATH="/media/files/code/kolodachan/server/src/"
+  export PYTHONPATH="$SCRIPT_DIR/src/"
 }
 
 custom_activate
