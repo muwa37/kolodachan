b08318098f0e69beb55c3802d29beb0ff8e82435
nkyume <nekoyume2003@gmail.com>
2024-05-19 17:52:36 +1000

    fixed activatevenv

--- server/activatevenv.sh
+++ server/activatevenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 custom_activate(){
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-  echo "Script directory: $SCRIPT_DIR"
+  
   source ".venv/bin/activate"
   export PYTHONPATH="$SCRIPT_DIR/src/"
 }
